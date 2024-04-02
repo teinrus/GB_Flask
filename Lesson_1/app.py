d4966da39738a30e74d55a70510902a14b7a2d97
EN <tnrkip@yandex.ru>
2024-04-01 22:51:49 -0300

    Lesson_2

--- Lesson_1/app.py
+++ Lesson_1/app.py
@@ -1,10 +1,28 @@
-from flask import Flask, redirect,render_template
+from flask import Flask, request,render_template, Response, make_response
 
 app = Flask(__name__) 
 
-@app.route('/')
+@app.route('/' , methods=['post', 'get'])
 def index():
     return render_template ('index.html')
+
+@app.route('/getcookie', methods = ['POST', 'GET'])
+def getcookie():
+    if request.method == 'POST':
+        user = request.form['username']
+        email = request.form['email']
+        resp = make_response(render_template('getcookie.html',user=user))
+        resp.set_cookie('userName', user)
+        resp.set_cookie('email', email)
+
+        return resp
+
+@app.route('/del', methods = ['POST', 'GET'])
+def delete_cookie():
+    resp = Response ("Удалить cookie")
+    resp = make_response(render_template('index.html'))
+    resp.delete_cookie('userName')
+    return resp
 
 @app.route('/Clothes')
 def Clothes():
